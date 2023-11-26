35b1075114a64f91a894621e4b81085e9c590bfe
Davi Gledson <davigledson@outlook.com>
2023-11-26 02:32:01 -0300

    O poder extra do Bind Mount

--- volumes/index.php
+++ volumes/index.php
@@ -6,7 +6,7 @@
     <title>Mensagem</title>
 </head>
 <body>
-    <h1>Escreva sua Mensagem</h1>
+    <h1>Escreva sua Mensagem! Bind Mount</h1>
 <form action="process.php" method="post">
     <input type="text" name="message" id="message">
     <input type="submit" value="Enviar Mensagem">
